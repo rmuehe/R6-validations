2e166c2ccae61f9538fcc627a545c2b950ce1633
rmuehe <rmuehe@gmail.com>
2021-12-03 18:47:02 -0800

    Update pending tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,15 +6,34 @@ RSpec.describe Customer, type: :model do
   it "is valid with valid attributes" do
     expect(subject).to be_valid
   end
-  it "is not valid without a last_name" do
-    subject.last_name=nil
+  it "is not valid without a first_name" do
+    subject.first_name = nil
     expect(subject).to_not be_valid
   end
-  it "is not valid without a phone number"
-  it "is not valid without an email"
-  it "is not valid if the phone number is not 10 chars"
-  it "is not valid if the phone number is not all digits"
-  it "is not valid if the email address doesn't have a @"
+  it "is not valid without a last_name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without a phone number" do
+    subject.phone = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid if the phone number is not 10 chars" do
+    subject.phone = "123456789"
+    expect(subject).to_not be_valid  
+  end
+  it "is not valid if the phone number is not all digits" do
+    subject.phone = "123123X123" 
+    expect(subject).to_not be_valid
+  end
+  it "is not valid if the email address doesn't have a @" do
+    subject.email = "jsmithsample.com"
+    expect(subject).to_not be_valid
+  end
   it "returns the correct full_name" do
     expect(subject.full_name).to eq ("Jack Smith")
   end
