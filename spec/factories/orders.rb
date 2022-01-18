e91630dc947f80b42eeb1c06169e2ba707e1118f
rmuehe <rmuehe@gmail.com>
2022-01-18 10:02:54 -0800

    Update order model tests & factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,7 +1,15 @@
+# FactoryBot.define do
+#   factory :order do
+#     product_name { "MyString" }
+#     product_count { 1 }
+#     customer { nil }
+#   end
+# end
+
 FactoryBot.define do
   factory :order do
-    product_name { "MyString" }
-    product_count { 1 }
-    customer { nil }
+    product_name {Faker::Lorem.word}
+    product_count {Faker::Number.number(digits: 3).to_i}
+    asspociation :customer
   end
 end
